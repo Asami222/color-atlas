5de8edd3dceab81499a7f7a526fefaadba148c43
Asami222 <asa2_pokecho@icloud.com>
2026-08-22 16:32:49 +0900

    FIX: PaletteForm_add_Spinner3

--- src/components/create/PaletteForm/PaletteForm.tsx
+++ src/components/create/PaletteForm/PaletteForm.tsx
@@ -252,11 +252,8 @@ const onSubmit = (data: CreateSchema) => {
         <IconLabel label="色彩" iconName="palette" htmlFor="colorData"/>
         <div className="flex justify-center items-center mx-auto w-full min-h-62 py-6 rounded-default bg-background-secondary">
         {isRestoring ? (
-          <div className="flex items-center justify-center">
-            <Spinner
-              size={24}
-              color="var(--color-border-disabled)"
-            />
+          <div className="text-red-500 text-xl font-bold">
+            RESTORING...
           </div>
           ) : ShapeComponent ? (
             <div className="w-50">
